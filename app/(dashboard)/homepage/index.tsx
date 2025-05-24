f8fea595ccbf3a8ea41fbac132c3fa2a1ecf3ae7
Aleksandar <aleksandar@e-vlasotince.info>
2025-05-24 12:26:13 +0200

    homepage user stat

--- app/(dashboard)/homepage/index.tsx
+++ app/(dashboard)/homepage/index.tsx
@@ -1,21 +1,34 @@
+import ErrorPage from "@/components/Error";
+import LoadingPage from "@/components/Loading";
 import Button from "@/components/ui/Button";
 import Headline from "@/components/ui/Headline";
 import Widget from "@/features/homepage/Widget";
+import { useProfile } from "@/features/profile/hooks/useProfile";
 import { useRouter } from "expo-router";
 import { Text, View } from "react-native";
 
 const HomePage = () => {
   const router = useRouter();
+  const { isPending, data, error } = useProfile();
+
+  if (isPending) {
+    return <LoadingPage />;
+  }
+
+  if (error) {
+    return <ErrorPage error={error} />;
+  }
+
   return (
     <View className="flex-1 pb-52 justify-between items-center px-4 py-6 gap-10 bg-background">
       <Headline level={2} className="text-center font-normal">
-        Dobrodošli <Text className="font-bold">Aleksandar Cvetkovic</Text>,
+        Dobrodošli <Text className="font-bold">{data?.user?.fullName}</Text>,
       </Headline>
       {/*  */}
       <View className="w-full h-[200px] relative">
         <Widget
           variant="secondary"
-          count={8}
+          count={data?.invoiceCount as number}
           position="left-10 -top-1"
           href="/invoices"
         >
@@ -23,7 +36,7 @@ const HomePage = () => {
         </Widget>
         <Widget
           variant="primary"
-          count={18}
+          count={data?.productsGaranteeCount as number}
           position="-bottom-2 right-8"
           href="/garanties"
         >
